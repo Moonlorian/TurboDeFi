15257d5d6159c055c050d0c547d24da136933536
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-02 15:17:23 +0200

    Updated transaction row component

--- mnlrn_swagger/src/pages/Scanner/Scanner.tsx
+++ mnlrn_swagger/src/pages/Scanner/Scanner.tsx
@@ -1,68 +1,84 @@
-import { TransactionRow } from "@multiversx/sdk-dapp/UI";
-import { Card, OutputContainer } from "components";
-import { useEffect, useState } from "react";
-import { getApiFullGeneric } from "services/apiQueries";
+import { TransactionRow } from '@multiversx/sdk-dapp/UI';
+import { Card, OutputContainer } from 'components';
+import { useEffect, useState } from 'react';
+import { getApiFullGeneric } from 'services/apiQueries';
+import { ScannerTransactionRow } from './widgets';
+import { getInterpretedTransaction } from '@multiversx/sdk-dapp/utils/transactions/getInterpretedTransaction';
 
 export const Scanner = () => {
+  const [transactionsList, setTransactionsList] = useState<any[]>([]);
+  const [filteredTransactionsList, setFilteredTransactionsList] = useState<
+    any[]
+  >([]);
 
-    const [transactionsList, setTransactionsList] = useState<any[]>([]);
-    const [filteredTransactionsList, setFilteredTransactionsList] = useState<any[]>([]);
-
-    const getTransactionsList = async (address: string) => {
-        //const list = await getApiFullGeneric(`accounts/${address}/transactions?after=1695636200&fields=txHash,receiver,timestamp,function`, { pageSize: 1000 });
-        //const list = await getApiFullGeneric(`accounts/${address}/transactions?after=1695636200`, { pageSize: 1000 });
-        const list = await getApiFullGeneric(`transactions?sender=erd1n3fzpwd9m6z35s9lqksxqnhmkyxatu20qp2lvtrdhdx6f4s8ypjsqskr84`, { pageSize: 1000 });
-        setTransactionsList(list);
-
-        const receiversList: string[] = [];
-        const filteredList = list.filter((transaction) => {
-            if (!receiversList.includes(transaction.receiver)) {
-                receiversList.push(transaction.receiver);
-                return true;
-            }
-            return false;
-        });
-        setFilteredTransactionsList(filteredList);
-
-        console.log(list);
-    }
-    useEffect(() => {
-        getTransactionsList("erd1n3fzpwd9m6z35s9lqksxqnhmkyxatu20qp2lvtrdhdx6f4s8ypjsqskr84");
-    }, []);
-
-    return (
-        <div className='flex flex-col gap-6 max-w-7xl w-full'>
-            <Card
-                className='flex-2'
-                key={'walletScanner'}
-                title={'Wallet Scanner'}
-                description={'Latest interactions of a wallet with different smart contracts'}
-                reference={''}
-            >
-                {transactionsList.map((transaction, index) => (
-                    <div key={index}>
-                        {transaction.receiver} | {transaction.function} | {transaction.timestamp}
-                    </div>
-                ))}
-                <br></br>
-                <br></br>
-                <br></br>
-                {filteredTransactionsList.map((transaction, index) => (
-                    <div key={index}>
-                        {transaction.receiver} | {transaction.function} | {transaction.timestamp}
-                    </div>
-                ))}
-                <br></br>
-                <br></br>
-                <br></br>
-                {filteredTransactionsList.map((transaction, index) => (
-                    <TransactionRow
-                        key={transaction.txHash}
-                        className='mx-transactions text-gray-500'
-                        transaction={transaction}
-                    />
-                ))}
-            </Card>
-        </div >
+  const getTransactionsList = async (address: string) => {
+    //const list = await getApiFullGeneric(`accounts/${address}/transactions?after=1695636200&fields=txHash,receiver,timestamp,function`, { pageSize: 1000 });
+    //const list = await getApiFullGeneric(`accounts/${address}/transactions?after=1695636200`, { pageSize: 1000 });
+    const list = await getApiFullGeneric(
+      `transactions?sender=erd1n3fzpwd9m6z35s9lqksxqnhmkyxatu20qp2lvtrdhdx6f4s8ypjsqskr84&withScResults=true`,
+      { pageSize: 50 }
     );
+    setTransactionsList(list);
+
+    const receiversList: string[] = [];
+    const filteredList = list.filter((transaction) => {
+      if (!receiversList.includes(transaction.receiver)) {
+        receiversList.push(transaction.receiver);
+        return true;
+      }
+      return false;
+    });
+    setFilteredTransactionsList(filteredList);
+
+    console.log(list);
+  };
+  useEffect(() => {
+    getTransactionsList(
+      'erd1n3fzpwd9m6z35s9lqksxqnhmkyxatu20qp2lvtrdhdx6f4s8ypjsqskr84'
+    );
+  }, []);
+
+  return (
+    <div className='flex flex-col gap-6 max-w-7xl w-full'>
+      <Card
+        className='flex-2'
+        key={'walletScanner'}
+        title={'Wallet Scanner'}
+        description={
+          'Latest interactions of a wallet with different smart contracts'
+        }
+        reference={''}
+      >
+        {transactionsList.map((transaction, index) => (
+          <div key={index}>
+            {transaction.receiver} | {transaction.function} |{' '}
+            {transaction.timestamp}
+          </div>
+        ))}
+        <br></br>
+        <br></br>
+        <br></br>
+        {filteredTransactionsList.map((transaction, index) => (
+          <div key={index}>
+            {transaction.receiver} | {transaction.function} |{' '}
+            {transaction.timestamp}
+          </div>
+        ))}
+        <br></br>
+        <br></br>
+        <br></br>
+        {filteredTransactionsList.map((transaction, index) => (
+          <ScannerTransactionRow
+            key={transaction.txHash}
+            className='mx-transactions text-gray-500'
+            transaction={getInterpretedTransaction({
+              address: '',
+              explorerAddress: '',
+              transaction
+            })}
+          />
+        ))}
+      </Card>
+    </div>
+  );
 };
--- mnlrn_swagger/src/pages/Scanner/widgets/ScannerTransactionRow.tsx
+++ mnlrn_swagger/src/pages/Scanner/widgets/ScannerTransactionRow.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import globalStyles from 'assets/sass/main.scss';
+//import globalStyles from 'assets/sass/main.scss';
 
-import styles from './transactionsTable.styles.scss';
-import { TimeAgo, TransactionMethod, TransactionReceiver, TransactionValue } from '@multiversx/sdk-dapp/UI';
-import { WithClassnameType, WithTransactionType } from '@multiversx/sdk-dapp/UI/types';
+//import styles from './transactionsTable.styles.scss';
+import {
+  TimeAgo,
+  TransactionMethod,
+  TransactionReceiver,
+  TransactionValue
+} from '@multiversx/sdk-dapp/UI';
+import {
+  WithClassnameType,
+  WithTransactionType
+} from '@multiversx/sdk-dapp/UI/types';
+import { getInterpretedTransaction } from '@multiversx/sdk-dapp/utils/transactions/getInterpretedTransaction';
 
 export interface TransactionRowPropsType
   extends WithTransactionType,
-  WithClassnameType {
+    WithClassnameType {
   showDirectionCol?: boolean;
   showLockedAccounts?: boolean;
 }
@@ -19,25 +28,24 @@ export const ScannerTransactionRow = ({
   transaction,
   showDirectionCol,
   showLockedAccounts
-}: TransactionRowPropsType) => (
-  <tr className={classNames(className, { new: transaction.isNew })}>
-    <td>
-      <TimeAgo value={transaction.timestamp} short tooltip />
-    </td>
+}: TransactionRowPropsType) => {
+  return (
+    <tr className={classNames(className, { new: transaction.isNew })}>
+      <td>
+        <TimeAgo value={transaction.timestamp} short tooltip />
+      </td>
 
-    <td>
-      <TransactionReceiver
-        transaction={transaction}
-        showLockedAccounts={showLockedAccounts}
-      />
-    </td>
+      <td>
+        <TransactionReceiver {...{ transaction, showLockedAccounts }} />
+      </td>
 
-    <td className={styles.transactionFunction}>
-      <TransactionMethod transaction={transaction} />
-    </td>
+      <td className=''>
+        <TransactionMethod transaction={transaction} />
+      </td>
 
-    <td className={globalStyles.textLeft}>
-      <TransactionValue transaction={transaction} />
-    </td>
-  </tr>
-);
+      <td className=''>
+        <TransactionValue {...{ transaction, hideMultipleBadge: true }} />
+      </td>
+    </tr>
+  );
+};
