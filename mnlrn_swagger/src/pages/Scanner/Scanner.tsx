5d28ad5ae3a2bc489a34b721da861fce4cc1cd05
Abel Alcaraz <abel.alcaraz@bitness.es>
2023-10-04 17:43:48 +0200

    scanner refactor

--- mnlrn_swagger/src/pages/Scanner/Scanner.tsx
+++ mnlrn_swagger/src/pages/Scanner/Scanner.tsx
@@ -11,7 +11,7 @@ export const Scanner = () => {
     const [transactionsList, setTransactionsList] = useState<any[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [filterPeriod, setFilterPeriod] = useState(0);
-    const [address, setAddress] = useState("erd14ntdu7kv44dztem5zluu934ekcj9sqmnmadl3zj7sxhjq6pyut6szd4dhv");
+    const [address, setAddress] = useState("");
 
     const scannerService = new ScannerService();
 
@@ -22,7 +22,7 @@ export const Scanner = () => {
         "From the beginning"
     ]
 
-    function calculateFilterDays () {
+    function calculateFilterDays() {
         switch (filterPeriod) {
             case 0:
                 return 7;
@@ -63,6 +63,78 @@ export const Scanner = () => {
         getTransactionsList();
     }, [filterPeriod]);
 
+    const ScannerFilter = () => {
+        return (
+            <div className='d-flex w-100 justify-content-between'>
+                <Form className='mb-2 d-flex w-75'>
+                    <Form.Group className='mr-3 w-100'>
+                        <Form.Control
+                            placeholder='Address'
+                            value={`${address}`}
+                            onChange={(e: any) => {
+                                updateAddress(e.target.value);
+                            }}
+                        />
+                    </Form.Group>
+                    <Button onClick={scanAddress} disabled={isLoading}>Scan</Button>
+                </Form>
+                <div className='text-end mb-2'>
+                    <Dropdown onSelect={handleFilterSelect}>
+                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                            {filterOptions[filterPeriod]}
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            {filterOptions.map((option, index) => (
+                                <Dropdown.Item
+                                    eventKey={index}
+                                    key={index}
+                                    active={filterPeriod == index}
+                                    disabled={isLoading}
+                                >
+                                    {option}
+                                </Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+            </div>
+        );
+    };
+
+    const TransactionsTable = () => {
+        return (
+            <table className='w-full divide-y divide-gray-200 overflow-auto table-auto'>
+                <thead className='bg-gray-50'>
+                    <tr>
+                        {COLUMNS.map((column) => (
+                            <th
+                                key={column}
+                                scope='col'
+                                className='px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/6'
+                            >
+                                {column}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody className='bg-white divide-y divide-gray-200'>
+                    {transactionsList.map((transaction, index) => (
+                        <ScannerTransactionRow
+                            key={transaction.txHash}
+                            className='mx-transactions text-gray-500'
+                            transaction={getInterpretedTransaction({
+                                address: '',
+                                explorerAddress: '',
+                                transaction
+                            })}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        );
+    }
+
     return (
         <div className='flex flex-col gap-6 max-w-7xl w-full'>
             <Card
@@ -74,70 +146,10 @@ export const Scanner = () => {
                 }
                 reference={''}
             >
-                <div className='d-flex w-100 justify-content-between'>
-                    <Form className='mb-2 d-flex w-75'>
-                        <Form.Group className='mr-3 w-100'>
-                            <Form.Control
-                                placeholder='Address'
-                                value={`${address}`}
-                                onChange={(e: any) => {
-                                    updateAddress(e.target.value);
-                                }}
-                            />
-                        </Form.Group>
-                        <Button onClick={scanAddress} disabled={isLoading}>Scan</Button>
-                    </Form>
-                    <div className='text-end mb-2'>
-                        <Dropdown onSelect={handleFilterSelect}>
-                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                                {filterOptions[filterPeriod]}
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                {filterOptions.map((option, index) => (
-                                    <Dropdown.Item
-                                        eventKey={index}
-                                        key={index}
-                                        active={filterPeriod == index}
-                                        disabled={isLoading}
-                                    >
-                                        {option}
-                                    </Dropdown.Item>
-                                ))}
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                </div>
+                <ScannerFilter />
                 <OutputContainer isLoading={isLoading} className='p-0'>
                     <div className='w-full h-full bg-gray-100 overflow-x-auto bg-white shadow rounded-lg'>
-                        <table className='w-full divide-y divide-gray-200 overflow-auto table-auto'>
-                            <thead className='bg-gray-50'>
-                                <tr>
-                                    {COLUMNS.map((column) => (
-                                        <th
-                                            key={column}
-                                            scope='col'
-                                            className='px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/6'
-                                        >
-                                            {column}
-                                        </th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody className='bg-white divide-y divide-gray-200'>
-                                {transactionsList.map((transaction, index) => (
-                                    <ScannerTransactionRow
-                                        key={transaction.txHash}
-                                        className='mx-transactions text-gray-500'
-                                        transaction={getInterpretedTransaction({
-                                            address: '',
-                                            explorerAddress: '',
-                                            transaction
-                                        })}
-                                    />
-                                ))}
-                            </tbody>
-                        </table>
+                        <TransactionsTable />
                     </div>
                 </OutputContainer>
             </Card>
