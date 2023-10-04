76111e4fc99a41e6c231b1064f2ba5b638a80907
Abel Alcaraz <abel.alcaraz@bitness.es>
2023-10-04 12:09:13 +0200

    css files order

--- mnlrn_swagger/src/index.tsx
+++ mnlrn_swagger/src/index.tsx
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
