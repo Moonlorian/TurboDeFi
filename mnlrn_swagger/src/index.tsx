b6f22abaa73a683241179c421b54d05afd9228f1
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-05 17:37:54 +0200

    Updated theme

--- mnlrn_swagger/src/index.tsx
+++ mnlrn_swagger/src/index.tsx
@@ -1,5 +1,7 @@
 import './styles/globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/scss/bootstrap.scss';
+import './assets/sass/theme.scss';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
