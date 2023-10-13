44a8a0576acee4c7acac70d340d5ab6797db8ed3
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-13 11:52:06 +0200

    Added graphql url

--- turboDefi/src/config/config.mainnet.ts
+++ turboDefi/src/config/config.mainnet.ts
@@ -6,6 +6,7 @@ export const contractAddress = '';
 export const API_URL = 'https://api.multiversx.com';
 export const GATEWAY_URL = 'https://api.multiversx.com';
 export const EXPLORER_URL = 'https://explorer.multiversx.com';
+export const GRAPHQL_URL = 'https://graph.xexchange.com/graphql';
 export const CHAIN_ID = '1';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.mainnet;
