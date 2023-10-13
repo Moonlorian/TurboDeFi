0d10dce9a2855c9188ab0c610683a0da29dae00f
Abel Alcaraz <abel.alcaraz@bitness.es>
2023-10-13 23:26:29 +0200

    move flows definition to Flow.tsx file

--- turboDefi/src/pages/Flow/Flow.tsx
+++ turboDefi/src/pages/Flow/Flow.tsx
@@ -1,5 +1,6 @@
 import { Card } from "components";
 import { FlowStep } from "./FlowStep";
+import { Flows } from "./Flows";
 
 type FlowType = {
     name: string,
@@ -27,61 +28,18 @@ export type FlowEndpointType = {
 
 export const Flow = () => {
 
-    const flowInfo: FlowType = {
-        "name": "flow-1",
-        "label": "Flow 1",
-        "description": "This flow will save a lot of time to the user",
-        "steps": [
-            {
-                "description": "Claim pending rewards from $CYBER Stake",
-                "endpoints": [
-                    {
-                        "project": "cyberpunkcity",
-                        "module": "cyber_token",
-                        "endpoint": "getAccountState"
-                    },
-                    {
-                        "project": "cyberpunkcity",
-                        "module": "cyber_token",
-                        "endpoint": "claim"
-                    }
-                ]
-            },
-            {
-                "description": "Claim pending rewards from CityNFT Stake",
-                "endpoints": [
-                    {
-                        "project": "cyberpunkcity",
-                        "module": "city_nft",
-                        "endpoint": "getAccountState"
-                    },
-                    {
-                        "project": "cyberpunkcity",
-                        "module": "city_nft",
-                        "endpoint": "claimReward"
-                    }
-                ]
-            },
-            {
-                "description": "Swap claimed $CYBER to whatever you want",
-                "component": "AshSwap",
-                "componentProps": {
-                    "defaultTokenFrom": "CYBER-489c1c"
-                }
-            }
-        ]
-    }
+    const flow: FlowType = Flows['flow_2'];
 
     return (
         <div className='flex flex-col gap-6 max-w-7xl w-full'>
             <Card
                 className='flex-2'
                 key={'flow'}
-                title={flowInfo.label}
-                description={flowInfo.description}
+                title={flow.label}
+                description={flow.description}
                 reference={''}
             >
-                {flowInfo.steps.map((step, index) => {
+                {flow.steps.map((step, index) => {
                     return <FlowStep step={step} key={index} index={index + 1} />
                 })}
             </Card>
