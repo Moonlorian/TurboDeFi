b2f5303093b8f23dfc647ed67bf8c8e2047e573d
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-13 10:24:41 +0200

    Styling

--- turboDefi/src/pages/AshSwap/AshSwap.tsx
+++ turboDefi/src/pages/AshSwap/AshSwap.tsx
@@ -1,6 +1,17 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
-import { aggregate, getTokenList, swap } from '../../services/ash/AshSwapService';
-import { Card, FormatAmount, Label, OutputContainer, TokenSelector } from 'components';
+import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons/faArrowsRotate';
+import {
+  aggregate,
+  getTokenList,
+  swap
+} from '../../services/ash/AshSwapService';
+import {
+  Card,
+  FormatAmount,
+  Label,
+  OutputContainer,
+  TokenSelector
+} from 'components';
 import { getUserTokensBalance } from 'services';
 import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks/account/useGetAccountInfo';
 import BigNumber from 'bignumber.js';
@@ -8,6 +19,7 @@ import { useGetTokenInfo } from 'hooks';
 import { formatAmount } from '@multiversx/sdk-dapp/utils/operations/formatAmount';
 import { Button } from 'react-bootstrap';
 import { ProjectEndpointForm } from 'pages/Project/ProjectInfo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const AshSwap = () => {
   const [tokenList, setTokenList] = useState<string[]>([]);
@@ -53,6 +65,9 @@ export const AshSwap = () => {
   };
 
   const swapTokenOrder = () => {
+    if (tokenFrom == '') return;
+    if (tokenTo == '') return;
+
     setAmountFrom(new BigNumber(0));
     setTokenFrom(tokenTo);
     setTokenTo(tokenFrom);
@@ -113,73 +128,113 @@ export const AshSwap = () => {
         reference={''}
       >
         <Card
-          className='flex-2 border w-50'
+          className='flex-2 border w-50 position-relative'
           key={''}
           title={''}
           description={''}
           reference={''}
         >
-          <div className=''>
-            <Label>From</Label>
-            <FormatAmount
-              value={balanceFrom.toFixed()}
-              token=''
-              decimals={
-                balanceFrom.isGreaterThan(0)
-                  ? tokenInfo.get(tokenFrom, 'decimals')
-                  : 0
-              }
-              digits={4}
-            />
+          <div className='bg-neutral-100 py-4 px-3'>
+            <div className=''>
+              <Label>From</Label>
+            </div>
+            <div className='position-relative flex flex-row'>
+              <TokenSelector
+                onChange={(tokenId: string) => changeToken('from', tokenId)}
+                isSearchable={true}
+                filter={getFilteredList(tokenTo)}
+                defaultValue={tokenFrom}
+                className='flex-1'
+              />
+              <span
+                className='text-sm cursor-pointer rounded-md position-absolute top-[15%] text-white right-[40%] main-color-bg main-color-30-border p-1'
+                onClick={() => {
+                  setAmountFrom(
+                    balanceFrom.dividedBy(
+                      10 ** tokenInfo.get(tokenFrom, 'decimals')
+                    )
+                  );
+                }}
+              >
+                max
+              </span>
+              <input
+                className='ms-2 text-end flex-1'
+                type='number'
+                placeholder='Amount'
+                value={amountFrom.isGreaterThan(0) ? amountFrom.toFixed() : ''}
+                onChange={(e: any) => {
+                  changeAmount('from', e.target.value);
+                }}
+              />
+            </div>
+            <div className='ms-2 flex justify-between me-2'>
+              <Label>
+                Balance:{' '}
+                <FormatAmount
+                  value={balanceFrom.toFixed()}
+                  token=''
+                  decimals={
+                    balanceFrom.isGreaterThan(0)
+                      ? tokenInfo.get(tokenFrom, 'decimals')
+                      : 0
+                  }
+                  digits={4}
+                />
+              </Label>
+              <Label>≈ 0$</Label>
+            </div>
           </div>
-          <div className=''>
-            <TokenSelector
-              onChange={(tokenId: string) => changeToken('from', tokenId)}
-              isSearchable={true}
-              filter={getFilteredList(tokenTo)}
-              defaultValue={tokenFrom}
-            />
-            <input
-              className='text-end'
-              type='number'
-              placeholder='Amount'
-              value={amountFrom.isGreaterThan(0) ? amountFrom.toFixed() : ''}
-              onChange={(e: any) => {
-                changeAmount('from', e.target.value);
-              }}
-            />
+          <div className='text-center'>
+            <a className='cursor-pointer' onClick={swapTokenOrder}>
+              <FontAwesomeIcon icon={faArrowsRotate} className='main-color' />
+            </a>
           </div>
-          <a onClick={swapTokenOrder}>change</a>
-          <div className=''>
-            <Label>To </Label>
-            <FormatAmount
-              value={balanceTo.toFixed()}
-              egldLabel=''
-              token=''
-              decimals={
-                balanceTo.isGreaterThan(0) ? tokenInfo.get(tokenTo, 'decimals') : 0
-              }
-              digits={4}
-            />
-          </div>
-          <div className=''>
-            <TokenSelector
-              onChange={(tokenId: string) => changeToken('to', tokenId)}
-              isSearchable={true}
-              filter={getFilteredList(tokenFrom)}
-              defaultValue={tokenTo}
-            />
-            <span>
-              {tokenTo
-                ? formatAmount({
-                  input: amountTo.toFixed(),
-                  decimals: tokenInfo.get(tokenTo, 'decimals'),
-                  digits: 4,
-                  addCommas: true,
-                  showLastNonZeroDecimal: false
-                })
-                : 0}
-            </span>
+          <div className='bg-neutral-100 py-4 px-3'>
+            <div className=''>
+              <Label>To</Label>
+            </div>
+            <div className='flex flex-row'>
+              <TokenSelector
+                onChange={(tokenId: string) => changeToken('to', tokenId)}
+                isSearchable={true}
+                filter={getFilteredList(tokenFrom)}
+                defaultValue={tokenTo}
+                className='flex-1'
+              />
+              <input
+                className='ms-2 text-end flex-1 pe-2 bg-transparent'
+                value={
+                  tokenTo
+                    ? formatAmount({
+                        input: amountTo.toFixed(),
+                        decimals: tokenInfo.get(tokenTo, 'decimals'),
+                        digits: 4,
+                        addCommas: true,
+                        showLastNonZeroDecimal: false
+                      })
+                    : 0
+                }
+                readOnly={true}
+              />
+            </div>
+            <div className='ms-2 flex justify-between me-2'>
+              <Label>
+                Balance:{' '}
+                <FormatAmount
+                  value={balanceTo.toFixed()}
+                  egldLabel=''
+                  token=''
+                  decimals={
+                    balanceTo.isGreaterThan(0)
+                      ? tokenInfo.get(tokenTo, 'decimals')
+                      : 0
+                  }
+                  digits={4}
+                />
+              </Label>
+              <Label>≈ 0$</Label>
+            </div>
           </div>
           <Button
             disabled={
@@ -192,7 +247,16 @@ export const AshSwap = () => {
                 .multipliedBy(10 ** tokenInfo.get(tokenFrom, 'decimals'))
                 .isGreaterThan(balanceFrom)
             }
-            onClick={() => swap(address, swapData.swaps, swapData.tokenAddresses, 1, new BigNumber(swapData.returnAmountWithDecimal))}
+            onClick={() =>
+              swap(
+                address,
+                swapData.swaps,
+                swapData.tokenAddresses,
+                1,
+                new BigNumber(swapData.returnAmountWithDecimal)
+              )
+            }
+            className='mt-3 main-color-bg main-color-30-border'
           >
             Swap
           </Button>
