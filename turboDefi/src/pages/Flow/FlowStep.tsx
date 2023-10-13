75fce8cb4b06f5819b43f4d606063e2a9657983e
Abel Alcaraz <abel.alcaraz@bitness.es>
2023-10-13 20:09:51 +0200

    remove unnecesary code

--- turboDefi/src/pages/Flow/FlowStep.tsx
+++ turboDefi/src/pages/Flow/FlowStep.tsx
@@ -1,7 +1,4 @@
-import StructReader from "StructReader/StructReader";
-import { environment } from "config";
 import { FlowStepType } from "./Flow";
-import { useEffect, useState } from "react";
 import { EndpointStep } from "./EndpointStep";
 import { ComponentStep } from "./ComponentStep";
 import { Card } from "components";
@@ -11,27 +8,6 @@ export const FlowStep = ({
 }: {
     step: FlowStepType
 }) => {
-/*
-    const [structReader, setStructReader] = useState<StructReader>();
-
-    const selectProject = async (selectedProject: string) => {
-        const newStructReader = new StructReader(
-            '/projects/' + environment + '/' + selectedProject.toLowerCase()
-        );
-        await newStructReader.load();
-        return newStructReader;
-    };
-
-    useEffect(() => {
-        if (step.project) {
-            selectProject(step.project).then((newStructReader: StructReader) => {
-                setStructReader(newStructReader);
-            });
-        }
-        return;
-    }, []);
-    */
-
     return (
         <Card
             className='flex-2 border mb-2'
