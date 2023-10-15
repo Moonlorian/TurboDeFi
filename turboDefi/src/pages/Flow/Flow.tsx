2741ebb2eaf39e387b28d15d9abce3005cc9471d
Abel Alcaraz <abel.alcaraz@bitness.es>
2023-10-15 19:48:35 +0200

    query flow endpoints info to sc

--- turboDefi/src/pages/Flow/Flow.tsx
+++ turboDefi/src/pages/Flow/Flow.tsx
@@ -1,6 +1,9 @@
 import { Card } from "components";
 import { FlowStep } from "./FlowStep";
 import { Flows } from "./Flows";
+import { useEffect, useState } from "react";
+import TurbodefiContractService from "services/TurbodefiContractService";
+import { API_URL } from "config";
 
 type FlowType = {
     name: string,
@@ -21,6 +24,7 @@ export type FlowStepType = {
 }
 
 export type FlowEndpointType = {
+    id: number
     project?: string
     module?: string
     endpoint?: string
@@ -28,21 +32,50 @@ export type FlowEndpointType = {
 
 export const Flow = () => {
 
-    const flow: FlowType = Flows['flow_2'];
+    const [flow, setFlow] = useState<FlowType>();
+
+    const turbodefiContractService = new TurbodefiContractService(API_URL);
+
+    const loadEndpoints = async () => {
+        const flow: FlowType = Flows['flow_1'];
+
+        for (let j = 0; j < flow.steps.length; j++) {
+            const step = flow.steps[j];
+            if (step.endpoints) {
+                const endpoints: FlowEndpointType[] = []
+                for (let i = 0; i < step.endpoints.length; i++) {
+                    const endpoint = await turbodefiContractService.getEndpointById(step.endpoints[i].id);
+                    endpoints.push(endpoint);
+                }
+                step.endpoints = endpoints;
+            }
+        };
+        return flow;
+    }
+
+    useEffect(() => {
+        loadEndpoints().then((updatedFlow: FlowType) =>
+            setFlow(updatedFlow)
+        );
+    }, []);
 
     return (
-        <div className='flex flex-col gap-6 max-w-7xl w-full'>
-            <Card
-                className='flex-2'
-                key={'flow'}
-                title={flow.label}
-                description={flow.description}
-                reference={''}
-            >
-                {flow.steps.map((step, index) => {
-                    return <FlowStep step={step} key={index} index={index + 1} />
-                })}
-            </Card>
-        </div>
+        <>
+            {flow && (
+                <div className='flex flex-col gap-6 max-w-7xl w-full'>
+                    <Card
+                        className='flex-2'
+                        key={'flow'}
+                        title={flow.label}
+                        description={flow.description}
+                        reference={''}
+                    >
+                        {flow.steps.map((step, index) => {
+                            return <FlowStep step={step} key={index} index={index + 1} />
+                        })}
+                    </Card>
+                </div>
+            )}
+        </>
     );
 }
---
+++ turboDefi/src/services/TurbodefiContractService.ts
@@ -0,0 +1,101 @@
+import { AbiRegistry, Address, ResultsParser, SmartContract, U64Value } from "@multiversx/sdk-core/out";
+import { ProxyNetworkProvider } from "@multiversx/sdk-network-providers/out";
+import { SC_ADDRESS } from "config";
+import { FlowEndpointType } from "pages/Flow";
+
+class TurbodefiContractService {
+
+    private gatewayUrl: string;
+
+    constructor(apiUrl: string) {
+        this.gatewayUrl = apiUrl;
+    }
+
+    async getEndpointById(id: number): Promise<FlowEndpointType> {
+        const decodeResponse = (data: any) => {
+            if (data == 1) {
+                return {
+                    id: data,
+                    project: "proteo".toLowerCase(),
+                    module: "Elite",
+                    endpoint: "user_info"
+                };
+            } else {
+                return {
+                    id: data,
+                    project: "proteo".toLowerCase(),
+                    module: "Elite",
+                    endpoint: "deposit"
+                };
+            }
+        }
+
+        const abi = AbiRegistry.create({
+            "endpoints": [
+                {
+                    "name": "getEndpointById",
+                    "mutability": "readonly",
+                    "inputs": [
+                        {
+                            "name": "id",
+                            "type": "u64"
+                        }
+                    ],
+                    "outputs": [
+                        {
+                            "type": "TdEndpointType"
+                        }
+                    ]
+                }
+            ],
+            "types": {
+                "TdEndpointType": {
+                    "type": "struct",
+                    "fields": [
+                        {
+                            "name": "project",
+                            "type": "bytes"
+                        },
+                        {
+                            "name": "module",
+                            "type": "bytes"
+                        },
+                        {
+                            "name": "endpoint",
+                            "type": "bytes"
+                        }
+                    ]
+                }
+            }
+        });
+
+        const provider = new ProxyNetworkProvider(
+            this.gatewayUrl,
+            { timeout: 5000 }
+        );
+        const endpointDefinition = abi.getEndpoint("getEndpointById");
+        const contract = new SmartContract({
+            address: new Address(SC_ADDRESS),
+            abi: abi
+        });
+        const query = contract.createQuery({
+            func: "getEndpointById",
+            args: [new U64Value(id)]
+        })
+        const queryResponse = await provider.queryContract(query);
+        let { firstValue } = new ResultsParser().parseQueryResponse(queryResponse, endpointDefinition);
+
+        const endpoint = {
+            id,
+            project: firstValue?.valueOf().project.valueOf().toString(),
+            module: firstValue?.valueOf().module.valueOf().toString(),
+            endpoint: firstValue?.valueOf().endpoint.valueOf().toString()
+        }
+        console.log("endpoint: ", endpoint);
+
+        return endpoint;
+        //return decodeResponse(id);
+    }
+}
+
+export default TurbodefiContractService;
