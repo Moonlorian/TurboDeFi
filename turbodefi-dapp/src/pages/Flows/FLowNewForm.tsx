d688e47a58ebaea2c1b25fd651edf54f0468bf31
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-17 13:37:04 +0200

    Create new Flow

--- turbodefi-dapp/src/pages/Flows/FLowNewForm.tsx
+++ turbodefi-dapp/src/pages/Flows/FLowNewForm.tsx
@@ -3,7 +3,10 @@ import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons/faFloppyDisk';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ActionButton, ActionButtonList } from 'components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { CreateFlow } from 'services';
+import { contractAddress } from 'config';
+import { useGetAccount } from '@multiversx/sdk-dapp/hooks/account/useGetAccount';
 
 type flowStatusType = 'idle' | 'creating' | 'saving';
 
@@ -14,23 +17,67 @@ export const FLowNewForm = ({
   onCancel: any;
   onFinish: any;
 }) => {
+  const [flowCreationStatus, setFlowCreationStatus] =
+    useState<flowStatusType>('idle');
   const [flowName, setFlowName] = useState('');
   const [flowLabel, setFlowLabel] = useState('');
   const [flowDescription, setFlowDescription] = useState('');
+  const [error, setError] = useState('');
+
+  const { address } = useGetAccount();
+
   const saveFlow = () => {
-    onFinish();
+    if (flowName == '') {
+      setError('You must speciy a flow name');
+      return;
+    }
+    if (flowLabel == '') {
+      setError('You must speciy a flow label');
+      return;
+    }
+    if (flowDescription == '') {
+      setError('You must speciy a flow description');
+      return;
+    }
+    CreateFlow(
+      contractAddress,
+      address,
+      flowName,
+      flowLabel,
+      flowDescription
+    );
+    //TODO. Make a transaction wathcer and set the 'creating' status to show spinner and wait
+
+    //setFlowCreationStatus('creating');
+    setFlowCreationStatus('saving');
   };
+
+  useEffect(() => {
+    if (flowCreationStatus == 'saving') {
+      onFinish();
+      //This function can be not neccesary
+      setFlowCreationStatus('idle');
+    }
+  }, [flowCreationStatus]);
+
   return (
-    <div className='ml-4 relative'>
+    <div className='ml-4 mb-6 relative'>
       <ActionButtonList>
-        <ActionButton action={onCancel}>
-          <FontAwesomeIcon icon={faFloppyDisk} />
-        </ActionButton>
-        <ActionButton action={saveFlow}>
-          <FontAwesomeIcon icon={faBan} />
-        </ActionButton>
+        {flowCreationStatus == 'creating' ? (
+          <p>please wait</p>
+        ) : (
+          <>
+            <ActionButton action={saveFlow}>
+              <FontAwesomeIcon icon={faFloppyDisk} />
+            </ActionButton>
+            <ActionButton action={onCancel}>
+              <FontAwesomeIcon icon={faBan} />
+            </ActionButton>
+          </>
+        )}
       </ActionButtonList>
-      <div className='pt-5'>
+      <h2 className="flex text-xl font-medium group text-uppercase">Create new Flow</h2>
+      <div className='pt-1'>
         <div className='mt-1 flex items-center pointer'>
           <FontAwesomeIcon
             icon={faArrowRight}
@@ -41,6 +88,7 @@ export const FLowNewForm = ({
             value={flowLabel}
             onChange={(e: any) => setFlowLabel(e.target.value)}
             placeholder='Insert Here flow Label'
+            required
           />
         </div>
         <div className='mt-1 flex items-center pointer'>
@@ -49,6 +97,7 @@ export const FLowNewForm = ({
             placeholder='Insert here flow name'
             value={flowName}
             onChange={(e: any) => setFlowName(e.target.value)}
+            required
           />
         </div>
         <div className='mt-1 flex items-center pointer'>
@@ -57,8 +106,10 @@ export const FLowNewForm = ({
             placeholder='Insert here flow description'
             value={flowDescription}
             onChange={(e: any) => setFlowDescription(e.target.value)}
+            required
           />
         </div>
+        <p className='text-red-500'>{error}</p>
       </div>
     </div>
   );
--- turbodefi-dapp/src/services/FlowsCreationService.ts
+++ turbodefi-dapp/src/services/FlowsCreationService.ts
@@ -1,12 +1,16 @@
-import { ContractFunction } from "@multiversx/sdk-core/out";
+import { ContractFunction, StringValue } from "@multiversx/sdk-core/out";
 import { sendTransaction } from "./contractInteractions";
 
-/*
-export const CreateFlow = async (address:string, flowName: string, flowDescription: string) => {
+
+export const CreateFlow = async (contract: string, address:string, flowName: string, flowLabel: string, flowDescription: string) => {
     await sendTransaction({
-        contractAddress: address,
-        functionName: new ContractFunction('claimRewards'),
-        sender
+        contractAddress: contract,
+        functionName: new ContractFunction('addFlow'),
+        sender: address,
+        args: [
+            new StringValue(flowName),
+            new StringValue(flowLabel),
+            new StringValue(flowDescription)
+        ]
     });
 };
-*/
