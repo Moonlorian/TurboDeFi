f9eff38c6cd0e9148f88d255ca7c81450828b401
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-18 17:39:55 +0200

    Load staking providers from local file

--- turbodefi-dapp/src/config/config.mainnet.ts
+++ turbodefi-dapp/src/config/config.mainnet.ts
@@ -7,7 +7,6 @@ export const API_URL = 'https://api.multiversx.com';
 export const GATEWAY_URL = 'https://api.multiversx.com';
 export const EXPLORER_URL = 'https://explorer.multiversx.com';
 export const GRAPHQL_URL = 'https://graph.xexchange.com/graphql';
-export const DELEGATION_URL = 'https://delegation-api.multiversx.com/providers';
 export const CHAIN_ID = '1';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.mainnet;
--- turbodefi-dapp/src/services/stakingProvidersLoadService.ts
+++ turbodefi-dapp/src/services/stakingProvidersLoadService.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { DELEGATION_URL, environment } from 'config';
+import { environment } from 'config';
 
 export const stakingProvidersLoadService = async () => {
   //https://delegation-api.multiversx.com/providers
-  return axios.get(DELEGATION_URL).then((response) => {
+  return axios.get(`/json/delegators.${environment}.json`).then((response) => {
     return response.data;
   });
 };
