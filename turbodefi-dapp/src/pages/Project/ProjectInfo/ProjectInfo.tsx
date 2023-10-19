dec491caef91cd93aafa4bb5b49b1bf3c881c103
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-19 13:02:14 +0200

    Back button in project page

--- turbodefi-dapp/src/pages/Project/ProjectInfo/ProjectInfo.tsx
+++ turbodefi-dapp/src/pages/Project/ProjectInfo/ProjectInfo.tsx
@@ -1,20 +1,24 @@
 import StructProject from 'StructReader/StructParts/StructProject';
-import { Card } from 'components';
+import { ActionButton, ActionButtonList, Card } from 'components';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
 
-export const ProjectInfo = ({
-  project
-}: {
-  project: StructProject;
-}) => {
-
+export const ProjectInfo = ({ project }: { project: StructProject }) => {
+  const navigate = useNavigate();
   return (
     <Card
-      className='flex-2'
+      className='flex-2 relative'
       key={'project_' + project.name}
       title={project.label || project.name}
       description={project.description}
       reference={project.url}
     >
+      <ActionButtonList>
+        <ActionButton action={() => {navigate('/project');}}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </ActionButton>
+      </ActionButtonList>
     </Card>
   );
 };
