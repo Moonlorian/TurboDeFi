bae4644a247cf239b13224de84c05ef70eb6a3c5
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-19 10:53:46 +0200

    in swap page, swap component are bigger

--- turbodefi-dapp/src/pages/AshSwap/AshSwapPage.tsx
+++ turbodefi-dapp/src/pages/AshSwap/AshSwapPage.tsx
@@ -1,9 +1,20 @@
-import { AshSwap } from 'components';
+import { AshSwap, Card } from 'components';
 
 export const AshSwapPage = () => {
   return (
-    <div className='m-auto mw-50'>
-      <AshSwap />
+    <div className='flex flex-col gap-6 max-w-7xl w-full'>
+      <div className='flex flex-col rounded-xl bg-white py-6 px-[4%] md:px-6 flex-2'>
+        <Card
+          className='flex-2 w-full position-relative'
+          title='Staking providers'
+          description='Manage your staked tokens'
+          reference={''}
+        >
+          <div className='w-[100%] lg:w-[50%] m-auto'>
+            <AshSwap />
+          </div>
+        </Card>
+      </div>
     </div>
   );
 };
