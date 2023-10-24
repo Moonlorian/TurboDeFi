b7fd5471a293110d52b938f47aac034fbf864080
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-24 14:16:32 +0200

    globaldataload doesn't load tokens price

--- turbodefi-dapp/src/services/GlobalDataLoad.tsx
+++ turbodefi-dapp/src/services/GlobalDataLoad.tsx
@@ -12,7 +12,7 @@ import { useGetPendingTransactions } from '@multiversx/sdk-dapp/hooks/transactio
 
 const globalData = {
   tokenList: {} as { [key: string]: any },
-  tokenBalance: {} as { [key: string]: any }z
+  tokenBalance: {} as { [key: string]: any }
 };
 
 export type GlobalDataTYpe = typeof globalData;
