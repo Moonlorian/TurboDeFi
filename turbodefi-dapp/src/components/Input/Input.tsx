6cfed6dd19c65e76d5228013027faac2943076c1
pedroCMoonlorian <pedro.carranza@moonlorian.com>
2023-10-25 10:02:27 +0200

    Removed border-0 classname

--- turbodefi-dapp/src/components/Input/Input.tsx
+++ turbodefi-dapp/src/components/Input/Input.tsx
@@ -4,7 +4,7 @@ export const Input = (props: any) => {
     <input
       {...props}
       className={classNames(
-        'placeholder-black/70 w-full py-[0.375rem] px-[0.75rem] text-base border rounded focus:border-blue-300 focus:shadow-[0_0_0_0.25rem_rgba(13,110,253,.25)] focus-visible:border-0',
+        'placeholder-black/70 w-full py-[0.375rem] px-[0.75rem] text-base border rounded focus:border-blue-300 focus:shadow-[0_0_0_0.25rem_rgba(13,110,253,.25)]',
         props?.className
       )}
     />
